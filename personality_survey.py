5bb671dea66289e8eaf56a1cbc58893ad19696f9
MrRosenfeld <32653211+MrRosenfeld@users.noreply.github.com>
2017-10-10 07:52:29 -0400

    First Commit

---
+++ personality_survey.py
@@ -0,0 +1,12 @@
+print("What's your favorite animal?")
+animal = input()
+
+if animal == "cat":
+    print("Woah, mine too!")
+elif animal == "dog":
+    print("That's my second fav!")
+elif animal == "parrot":
+    print("Really? OK.")
+else:
+    print(animal + " sounds cool.")
+
